2211bbe28cddf041c4f3984361536d69fdec261e
Kevin <bakarakevin679@gmail.com>
2025-07-08 12:23:16 +0700

    Fixing UI on AR Scene

--- Assets/Scripts/arscene.cs
+++ Assets/Scripts/arscene.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class arscene : MonoBehaviour
 {
     void Start()
     {
         Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MenuUtama");
     }
 
 }
